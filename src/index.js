e4c5607e48b35fc9ce3938b421295d12815a9ae8
johnshields <johndshields14@gmail.com>
2025-07-31 11:11:10 +1000

    feat: Added Swagger docs to API.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const equipmentRoutes = require('./routes/gearRoutes');
+const gearRoutes = require('./routes/gearRoutes');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./src/docs/swagger.yaml');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
 
-app.use('/api/gear', equipmentRoutes);
+// Swagger API docs
+app.use('/api', swaggerUi.serve, (req, res, next) => swaggerUi.setup(swaggerDocument)(req, res, next));
+
+// Route setup
+app.use('/api/gear', gearRoutes);
 
 // Load balancer
 app.get('/', (req, res) => {
     res.json({status: 'modulr API is live...'});
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`modulr API running on port ${PORT}`);
 });
